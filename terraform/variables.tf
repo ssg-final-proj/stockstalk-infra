c6ddb886c032183da61d32833abcf1121728ddb5
A-Hyeon <117140125+ahastuart@users.noreply.github.com>
2025-03-01 08:36:20 +0900

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -9,3 +9,21 @@ variable "vpc_cidr" {
   type        = string
   default     = "10.0.0.0/16"
 }
+
+variable "public_subnets" {
+  description = "List of public subnet CIDRs"
+  type        = map(string)
+  default = {
+    "subnet_1" = "10.0.1.0/24"
+    "subnet_2" = "10.0.2.0/24"
+  }
+}
+
+variable "private_subnets" {
+  description = "List of private subnet CIDRs"
+  type        = map(string)
+  default = {
+    "subnet_1" = "10.0.3.0/24"
+    "subnet_2" = "10.0.4.0/24"
+  }
+}
