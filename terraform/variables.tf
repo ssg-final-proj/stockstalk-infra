55e9eb1ce68339239ced0c3f31d80d57ef7af6f5
A-Hyeon <117140125+ahastuart@users.noreply.github.com>
2025-03-01 08:38:58 +0900

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -27,3 +27,42 @@ variable "private_subnets" {
     "subnet_2" = "10.0.4.0/24"
   }
 }
+
+variable "rds_subnets" {
+  description = "List of RDS subnet CIDRs"
+  type        = map(string)
+  default = {
+    "subnet_1" = "10.0.5.0/24"
+    "subnet_2" = "10.0.6.0/24"
+  }
+}
+
+variable "rds_instance_class" {
+  description = "RDS instance type"
+  type        = string
+  default     = "db.t3.micro"
+}
+
+variable "rds_storage" {
+  description = "RDS allocated storage in GB"
+  type        = number
+  default     = 20
+}
+
+variable "rds_engine" {
+  description = "RDS database engine"
+  type        = string
+  default     = "mysql"
+}
+
+variable "rds_engine_version" {
+  description = "RDS engine version"
+  type        = string
+  default     = "8.0"
+}
+
+variable "rds_username" {
+  description = "RDS master username"
+  type        = string
+  default     = "admin"
+}
