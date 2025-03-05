77fc8de062750b368b5fcfdba93fe058446b9037
HJ <heejnam0918@gmail.com>
2025-03-05 20:20:57 +0900

    Update variables.tf

--- terraform-vpc/variables.tf
+++ terraform-vpc/variables.tf
@@ -80,16 +80,6 @@ variable "rds_storage_type" {
 }
 
 ################ bastion.tf ################
-variable "private_key_path" {
-  description = "생성된 콘솔 서버용 키 파일 저장 경로"
-  type        = string
-}
-
-variable "file_permission" {
-  description = "파일에 적용할 POSIX 권한"
-  type        = string
-}
-
 variable "bastion_ami_id" {
   description = "콘솔 서버용 Amazon Linux 2 AMI ID"
   type        = string
