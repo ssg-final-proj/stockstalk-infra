e7d158f8933968ea3953bd0a039844ff508e5974
HJ <heejnam0918@gmail.com>
2025-03-08 12:28:16 +0900

    Delete terraform-vpc directory

--- terraform-vpc/backend_vpc.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "tf-terraform-state-bucket-apucpa4m"
-    key            = "terraform/vpc/terraform.tfstate"
-    region         = "ap-northeast-2"
-    encrypt        = true
-    dynamodb_table = "tf-terraform-lock-table"
-  }
-}
