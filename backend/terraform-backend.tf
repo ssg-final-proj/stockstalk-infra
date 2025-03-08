d205d87500a64b1d391bab27e1734c4eeb2d0e83
A-Hyeon <117140125+ahastuart@users.noreply.github.com>
2025-03-08 17:11:00 +0900

    Update and rename backend_infra.tf to terraform-backend.tf

--- backend/terraform-backend.tf
+++ backend/terraform-backend.tf
@@ -1,8 +1,6 @@
-[terraform@ip-192-168-20-77 terraform-infra]$ cat backend_infra.tf
-# terraform_infra.tf
 terraform {
   backend "s3" {
-    bucket         = "tf-terraform-state-bucket-xvpie8v5"  # S3 버킷 이름 output 보고 수정
+    bucket         = "tf-terraform-state-bucket-vss-2025"  # S3 버킷 이름 output 보고 수정
     key            = "terraform/infra/terraform.tfstate"
     region         = "ap-northeast-2"
     dynamodb_table = "tf-terraform-lock-table"             # DynamoDB 테이블 이름 입력
