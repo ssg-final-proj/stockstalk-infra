8b40310b1f85d2869876ca8f51ab193202e055d1
A-Hyeon <117140125+ahastuart@users.noreply.github.com>
2025-03-08 17:06:53 +0900

    Update and rename output.tf to output.tf

--- backend/output.tf
+++ backend/output.tf
@@ -1,8 +1,8 @@
 # 랜덤으로 생성된 S3 버킷 이름 출력 : tf-terraform-state-bucket-XXXXXXXX
-output "s3_bucket_name" {
-  description = "The name of the created S3 bucket"
-  value       = aws_s3_bucket.tf_terraform_state_bucket.bucket
-}
+# output "s3_bucket_name" {
+#   description = "The name of the created S3 bucket"
+#   value       = aws_s3_bucket.tf_terraform_state_bucket.bucket
+# }
 
 # S3 버킷 리전 출력
 output "s3_bucket_region" {
