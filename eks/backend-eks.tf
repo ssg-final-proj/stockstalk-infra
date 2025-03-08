771aeff311a4fc0774eb1835a2e168b62dd2e0d9
A-Hyeon <117140125+ahastuart@users.noreply.github.com>
2025-03-08 18:38:05 +0900

    Create backend-eks.tf

---
+++ eks/backend-eks.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket         = "tf-terraform-state-bucket-vss-2025"
+    key            = "terraform/eks/terraform.tfstate"
+    region         = "ap-northeast-2"
+    encrypt        = true
+    dynamodb_table = "tf-terraform-lock-table"
+  }
+}
