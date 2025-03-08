7b6108e9f7a5b4c0c2d820d9cddfbc14ef16f251
A-Hyeon <117140125+ahastuart@users.noreply.github.com>
2025-03-08 18:16:01 +0900

    Update and rename backend_vpc.tf to backend_network.tf

--- network/backend_network.tf
+++ network/backend_network.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket         = "tf-terraform-state-bucket-xvpie8v5"
-    key            = "terraform/vpc/terraform.tfstate"
+    bucket         = "tf-terraform-state-bucket-vss-2025"
+    key            = "terraform/network/terraform.tfstate"
     region         = "ap-northeast-2"
     encrypt        = true
     dynamodb_table = "tf-terraform-lock-table"
