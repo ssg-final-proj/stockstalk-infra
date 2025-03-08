33e0e780d0326c813a8eebe8405d3f1461b79235
HJ <heejnam0918@gmail.com>
2025-03-08 12:30:32 +0900

    Update backend_addons.tf

--- terraform-addons/backend_addons.tf
+++ terraform-addons/backend_addons.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "tf-terraform-state-bucket-jrn63fgd"
+    bucket         = "tf-terraform-state-bucket-xvpie8v5"
     key            = "terraform/addons/terraform.tfstate"
     region         = "ap-northeast-2"
     encrypt        = true
