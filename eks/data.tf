c240531575c11d513e5a25bffa36de39982d98f9
A-Hyeon <117140125+ahastuart@users.noreply.github.com>
2025-03-08 18:47:30 +0900

    Update data.tf

--- eks/data.tf
+++ eks/data.tf
@@ -3,8 +3,8 @@ data "terraform_remote_state" "network" {
   backend = "s3"
 
   config = {
-    bucket = "tf-terraform-state-bucket-vss-2025"
-    key    = "terraform/network/terraform.tfstate"
-    region = "ap-northeast-2"
+    bucket = var.s3_bucket_name
+    key    = var.s3_bucket_key
+    region = var.s3_bucket_region
   }
 }
